092819610c7a7bd44970ab496327f29701035e04
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2025-05-01 12:49:37 -0400

    feat: enable starship

--- hosts/john-nix-05/home.nix
+++ hosts/john-nix-05/home.nix
@@ -72,6 +72,10 @@
       ];
       theme = "robbyrussell";
     };
+
+    initContent = ''
+      eval "$(starship init zsh)"
+    '';
   };
 
   # Zoxide
@@ -82,6 +86,70 @@
     options = [
       "--cmd cd"
     ];
+  };
+
+  # Starship
+  programs.starship = {
+    enable = true;
+    settings = {
+      format = ''
+        $time\
+        $directory\
+        $python\
+        $rust\
+        $zig\
+        $git_branch\
+        $git_commit\
+        $git_state\
+        $git_status\
+        $cmd_duration\
+        $character\
+      '';
+      add_newline = false;
+
+      time = {
+        format = "[\\[$time\\]](\$style) ";
+        style = "cyan";
+        disabled = false;
+      };
+
+      directory = {
+        format = "[\\[$path\\]](\$style) ";
+        style = "cyan";
+        truncation_symbol = ".../";
+        truncate_to_repo = false;
+      };
+
+      cmd_duration = {
+        min_time = 50;
+        show_milliseconds = true;
+        style = "yellow";
+      };
+
+      python = {
+        format = "[\\((\$version)( \$virtualenv)\\)](\$style) ";
+        style = "green";
+      };
+
+      rust = {
+        format = "[\\((\$version)\\)](\$style) ";
+        style = "red";
+      };
+
+      zig = {
+        format = "[\\((\$version)\\)](\$style) ";
+        style = "yellow";
+      };
+
+      git_branch = {
+        format = "[\\[$branch(:\$remote_branch)\\]](\$style) ";
+        style = "purple";
+      };
+
+      git_status = {
+        style = "bold purple";
+      };
+    };
   };
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
