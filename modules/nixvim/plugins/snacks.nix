3165a5e89e5e2d6990186bde590d4a1249cdc2d1
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2025-05-27 15:22:28 -0700

    feat: add snacks
    patch: update snacks config

---
+++ modules/nixvim/plugins/snacks.nix
@@ -0,0 +1,10 @@
+{
+  programs.nixvim.plugins.snacks = {
+    enable = true;
+    settings = {
+      indent.enabled = true;
+      profiler.enabled = true;
+      scroll.enabled = true;
+    };
+  };
+}
