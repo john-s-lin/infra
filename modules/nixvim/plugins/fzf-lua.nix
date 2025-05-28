8c30d142e9a6a575f2dcd55e601a885c832bc4e6
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2025-05-27 18:00:35 -0700

    feat: add init keymaps

--- modules/nixvim/plugins/fzf-lua.nix
+++ modules/nixvim/plugins/fzf-lua.nix
@@ -2,5 +2,29 @@
   programs.nixvim.plugins.fzf-lua = {
     enable = true;
     profile = "fzf-native";
+
+    keymaps = {
+      "<leader> " = {
+        action = "files";
+        options = {
+          silent = true;
+          desc = "Find Files";
+        };
+      };
+      "<leader>/" = {
+        action = "live_grep";
+        options = {
+          silent = true;
+          desc = "Grep";
+        };
+      };
+      "<leader>," = {
+        action = "buffers";
+        options = {
+          silent = true;
+          desc = "Buffers";
+        };
+      };
+    };
   };
 }
