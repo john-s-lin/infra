07cae64b1d3b2254ecb41d7a19689d3463c04235
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2025-06-23 23:26:34 -0700

    feat: custom PAM

--- modules/services/fingerprint.nix
+++ modules/services/fingerprint.nix
@@ -1,4 +1,9 @@
-{ ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   systemd.services.fprintd = {
     wantedBy = [ "multi-user.target" ];
@@ -6,4 +11,25 @@
   };
 
   services.fprintd.enable = true;
+
+  security.pam.services.login.fprintAuth = false;
+  security.pam.services.gdm-fingerprint = lib.mkIf (config.services.fprintd.enable) {
+    text = ''
+      auth       required                    pam_shells.so
+      auth       requisite                   pam_nologin.so
+      auth       requisite                   pam_faillock.so      preauth
+      auth       required                    ${pkgs.fprintd}/lib/security/pam_fprintd.so
+      auth       optional                    pam_permit.so
+      auth       required                    pam_env.so
+      auth       [success=ok default=1]      ${pkgs.gdm}/lib/security/pam_gdm.so
+      auth       optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so
+
+      account    include                     login
+
+      password   required                    pam_deny.so
+
+      session    include                     login
+      session    optional                    ${pkgs.gnome-keyring}/lib/security/pam_gnome_keyring.so auto_start
+    '';
+  };
 }
