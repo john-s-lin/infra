38d348277ae904601d74e4d5bf553fa54c09aa80
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2025-06-28 19:11:21 -0700

    fix: remove let home-manager

--- hosts/john-nix-05/configuration.nix
+++ hosts/john-nix-05/configuration.nix
@@ -9,9 +9,6 @@
   inputs,
   ...
 }:
-let
-  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/master.tar.gz;
-in
 {
   imports = [
     # Include the results of the hardware scan.
