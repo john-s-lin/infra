c80cf1025bc614e77734e2eb3969113f5f703d81
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2025-06-28 21:23:05 -0700

    patch: reenable rclone

--- hosts/john-nix-05/home.nix
+++ hosts/john-nix-05/home.nix
@@ -12,7 +12,7 @@
     ../../modules/packages/work.nix
 
     # Cloud - OneDrive, GDrive
-    # ../../modules/home/cloud.nix
+    ../../modules/home/cloud.nix
 
     # Config
     ../../modules/home/bat.nix
