1cb4c343de41e5a424660b940329c7721f7c9b1e
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2025-08-18 14:06:22 -0700

    feat: init symlinks to existing dotfiles

--- hosts/john-air-03/home.nix
+++ hosts/john-air-03/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 {
   # Set user-specific environment variables here. This will override the system.
   home.sessionVariables = {
@@ -13,6 +13,29 @@
   # It is good practice to manage your shell with Home Manager.
   # For example, for zsh:
   programs.zsh.enable = true;
+
+  home.file = {
+    ".config" = {
+      source = "${inputs.dotfiles}/config";
+      recursive = true;
+    };
+    ".bashrc" = {
+      source = "${inputs.dotfiles}/.bashrc";
+    };
+    ".zshrc" = {
+      source = "${inputs.dotfiles}/.zshrc";
+    };
+    ".zshenv" = {
+      text = ''
+        . "$HOME/.cargo/env"
+
+        # ZVM
+        export ZVM_INSTALL="$HOME/.zvm/self"
+        export PATH="$PATH:$HOME/.zvm/bin"
+        export PATH="$PATH:$ZVM_INSTALL/"
+      '';
+    };
+  };
 
   # Set your Home Manager state version.
   home.stateVersion = "25.05"; # Or your current version
