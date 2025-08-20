0706614150b21b15abcfb07070f94d9b8b9fe5bb
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2025-08-20 11:35:19 -0700

    patch: set Zellij data dir to be local

--- modules/home/zoxide.nix
+++ modules/home/zoxide.nix
@@ -1,5 +1,9 @@
 { ... }:
 {
+  home.sessionVariables = {
+    _ZO_DATA_DIR = "/Users/john.lin/.local/share/zoxide";
+  };
+
   programs.zoxide = {
     enable = true;
     enableBashIntegration = true;
