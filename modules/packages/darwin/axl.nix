412a780bd1752c05f42f7c6f814072223f9a24b1
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2025-09-02 23:28:08 -0400

    feat: move all ai-related packages to their own module (#24)

--- modules/packages/darwin/axl.nix
+++ modules/packages/darwin/axl.nix
@@ -2,15 +2,9 @@
   homebrew = {
     casks = [
       "1password"
-      "cursor"
       "slack"
-
-    ];
-    taps = [
-      "sst/homebrew-tap"
     ];
     brews = [
-      "sst/tap/opencode"
       "uv"
     ];
   };
