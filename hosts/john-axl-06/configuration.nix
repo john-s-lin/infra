0f26a8f78c369e55c0268f52172950156147c6ec
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2025-09-04 12:18:36 -0400

    feat: nix-darwin defaults

--- hosts/john-axl-06/configuration.nix
+++ hosts/john-axl-06/configuration.nix
@@ -18,9 +18,33 @@
     home = "/Users/john";
   };
 
-  system.defaults.dock = {
-    orientation = "left";
-    tilesize = 36;
+  system.defaults = {
+    dock = {
+      orientation = "left";
+      tilesize = 36;
+    };
+
+    WindowManager = {
+      EnableTilingByEdgeDrag = false;
+      EnableTiledWindowMargins = false;
+      EnableTopTilingByEdgeDrag = false;
+      EnableTilingOptionAccelerator = false;
+    };
+
+    finder = {
+      FXPreferredViewStyle = "Nlsv";
+      ShowPathbar = true;
+      ShowStatusBar = true;
+    };
+
+    NSGlobalDomain = {
+      AppleShowAllExtensions = true;
+    };
+
+    ".GlobalPreferences" = {
+      # -1.0 is no mouse acceleration
+      "com.apple.mouse.scaling" = -1.0;
+    };
   };
 
   system.primaryUser = "john";
