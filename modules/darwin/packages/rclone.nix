ab8009b78a0b56ee2f90e5383c1df98f4d9c6b23
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2025-09-10 10:32:54 -0400

    feat: add rclone conf at launch

--- modules/darwin/packages/rclone.nix
+++ modules/darwin/packages/rclone.nix
@@ -1,4 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
+let
+  username = "john";
+  homeDir = config.users.users.${username}.home;
+in
 {
   environment.systemPackages = with pkgs; [
     rclone
@@ -6,5 +10,40 @@
 
   homebrew = {
     casks = [ "macfuse" ];
+  };
+
+  launchd.user.agents = {
+    rclone-onedrive = {
+      serviceConfig = {
+        ProgramArguments = [
+          "/bin/sh"
+          "-c"
+          "mkdir -p ${homeDir}/OneDrive && ${pkgs.rclone}/bin/rclone --vfs-cache-mode writes --config=${homeDir}/.config/rclone/rclone.conf --ignore-checksum mount OneDrive: ${homeDir}/OneDrive"
+        ];
+        RunAtLoad = true;
+        KeepAlive = {
+          Crashed = true;
+          SuccessfulExit = false;
+        };
+        StandardOutPath = "${homeDir}/Library/Logs/rclone-onedrive.log";
+        StandardErrorPath = "${homeDir}/Library/Logs/rclone-onedrive.err.log";
+      };
+    };
+    rclone-gdrive = {
+      serviceConfig = {
+        ProgramArguments = [
+          "/bin/sh"
+          "-c"
+          "mkdir -p ${homeDir}/GDrive && ${pkgs.rclone}/bin/rclone --vfs-cache-mode writes --config=${homeDir}/.config/rclone/rclone.conf --ignore-checksum mount GDrive: ${homeDir}/GDrive"
+        ];
+        RunAtLoad = true;
+        KeepAlive = {
+          Crashed = true;
+          SuccessfulExit = false;
+        };
+        StandardOutPath = "${homeDir}/Library/Logs/rclone-gdrive.log";
+        StandardErrorPath = "${homeDir}/Library/Logs/rclone-gdrive.err.log";
+      };
+    };
   };
 }
