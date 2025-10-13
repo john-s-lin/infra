36c20fa680d7cf1bca9c293a8cea41b5bed8d0e7
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2025-10-12 21:23:06 -0400

    feat: pin vscode to workspace 6

--- modules/home/hypr.nix
+++ modules/home/hypr.nix
@@ -215,6 +215,7 @@ in
         "workspace 3, class:obsidian"
         "workspace 4, class:zen-twilight"
         "workspace 5, class:dev.zed.Zed"
+        "workspace 6, class:Code"
         "suppressevent maximize, class:.*"
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
       ];
