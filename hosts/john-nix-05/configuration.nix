e61fedd2c19a68d97c52a9c461b6a7322646ac78
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2025-11-25 10:11:55 -0500

    feat: add localBin to path for uv on nixOS

--- hosts/john-nix-05/configuration.nix
+++ hosts/john-nix-05/configuration.nix
@@ -59,6 +59,9 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  # Path
+  environment.localBinInPath = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.john = {
     isNormalUser = true;
