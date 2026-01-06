f3ae291c0b511d4739e718dc558ce19dbeceae0a
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2026-01-06 00:18:59 -0500

    feat: change tz to Shanghai

--- hosts/john-nix-05/configuration.nix
+++ hosts/john-nix-05/configuration.nix
@@ -40,7 +40,7 @@
   };
 
   # Set your time zone.
-  time.timeZone = "America/New_York";
+  time.timeZone = "Asia/Shanghai";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
