d41ead51117c84c32d589065399fe3282c04992d
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2026-02-06 11:24:49 -0500

    fix: zellij config copy command for osx

--- modules/home/zellij.nix
+++ modules/home/zellij.nix
@@ -7,7 +7,7 @@
     settings = {
       theme = "tokyo-night-dark";
       pane_frames = true;
-      copy_command = if pkgs.stdenv.isLinux then "wl-copy" else null;
+      copy_command = if pkgs.stdenv.isLinux then "wl-copy" else "pbcopy";
       show_startup_tips = false;
       scroll_buffer_size = 100000;
     };
