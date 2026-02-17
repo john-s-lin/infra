81651be0e6dda93b99beb55287e8bedf8d7af310
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2026-02-16 19:40:00 -0500

    Remove redundant git.auto-local-bookmark from jj config
    This is the default behavior in jj.

--- modules/home/jujutsu.nix
+++ modules/home/jujutsu.nix
@@ -4,12 +4,6 @@
     enable = true;
 
     settings = {
-      # Git interoperability settings
-      git = {
-        # Automatically create local bookmarks for Git branches
-        auto-local-bookmark = true;
-      };
-
       # Enable color output
       ui.color = "auto";
     };
