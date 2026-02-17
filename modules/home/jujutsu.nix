30a5f2da737d707882bb68804cd84f0b41b8845c
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2026-02-16 19:38:38 -0500

    Remove redundant ui.editor from jj config
    jj uses $VISUAL > $EDITOR, which Helix already sets.

--- modules/home/jujutsu.nix
+++ modules/home/jujutsu.nix
@@ -4,9 +4,6 @@
     enable = true;
 
     settings = {
-      # Use Helix as the default editor
-      ui.editor = "hx";
-
       # Git interoperability settings
       git = {
         # Automatically create local bookmarks for Git branches
