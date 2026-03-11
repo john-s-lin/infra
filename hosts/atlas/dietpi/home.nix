99e0d71a404a910223a2de62806c400a25bcd2ea
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2026-03-11 00:58:20 -0400

    patch: fix imports

--- hosts/atlas/dietpi/home.nix
+++ hosts/atlas/dietpi/home.nix
@@ -1,16 +1,16 @@
 {
   imports = [
     # Server tools (previously in server.nix)
-    ../../modules/home/bat.nix
-    ../../modules/home/bottom.nix
-    ../../modules/home/direnv.nix
-    ../../modules/home/git.nix
-    ../../modules/home/jujutsu.nix
-    ../../modules/home/helix.nix
-    ../../modules/home/zsh.nix
+    ../../../modules/home/bat.nix
+    ../../../modules/home/bottom.nix
+    ../../../modules/home/direnv.nix
+    ../../../modules/home/git.nix
+    ../../../modules/home/jujutsu.nix
+    ../../../modules/home/helix.nix
+    ../../../modules/home/zsh.nix
 
     # Terminal packages
-    ../../modules/packages/terminal.nix
+    ../../../modules/packages/terminal.nix
   ];
 
   home.username = "dietpi";
--- hosts/heimdall/john/home.nix
+++ hosts/heimdall/john/home.nix
@@ -2,20 +2,20 @@
 {
   imports = [
     # Server tools (previously in server.nix)
-    ../../modules/home/bat.nix
-    ../../modules/home/bottom.nix
-    ../../modules/home/direnv.nix
-    ../../modules/home/git.nix
-    ../../modules/home/jujutsu.nix
-    ../../modules/home/helix.nix
-    ../../modules/home/zsh.nix
+    ../../../modules/home/bat.nix
+    ../../../modules/home/bottom.nix
+    ../../../modules/home/direnv.nix
+    ../../../modules/home/git.nix
+    ../../../modules/home/jujutsu.nix
+    ../../../modules/home/helix.nix
+    ../../../modules/home/zsh.nix
 
     # AI/development packages
-    ../../modules/packages/ai.nix
-    ../../modules/home/opencode.nix
+    ../../../modules/packages/ai.nix
+    ../../../modules/home/opencode.nix
 
     # Terminal packages
-    ../../modules/packages/terminal.nix
+    ../../../modules/packages/terminal.nix
   ];
 
   home.username = "john";
