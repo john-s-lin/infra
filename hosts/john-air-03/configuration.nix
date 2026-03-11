6b35879ca2569319284d2c1107f4aae3a9f71ebc
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2026-03-11 14:21:23 -0400

    fix: renamed package not called

--- hosts/john-air-03/configuration.nix
+++ hosts/john-air-03/configuration.nix
@@ -2,7 +2,7 @@
   imports = [
     ../../modules/fonts.nix
     ../../modules/packages/default.nix
-    ../../modules/darwin/packages/common.nix
+    ../../modules/darwin/packages/homebrew.nix
     ../../modules/darwin/packages/personal.nix
     ../../modules/darwin/packages/ai.nix
     ../../modules/darwin/packages/rclone.nix
