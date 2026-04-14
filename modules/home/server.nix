8ca59ff9b331032d01a421c3915c6d29dc6c08c1
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2026-04-14 06:30:42 +0000

    patch: update PS1 prompt

--- modules/home/server.nix
+++ modules/home/server.nix
@@ -4,16 +4,16 @@
     if [ -f ~/.bash_aliases ]; then
       . ~/.bash_aliases
     fi
-    PROMPT="%{$fg_bold[yellow]%}%n@%m%{$reset_color%} $PROMPT"
+    PROMPT="%{$fg_bold[yellow]%}$USER@%m%{$reset_color%} $PROMPT"
   '';
 
   programs.bash = {
     enable = true;
     initExtra = ''
-      if [-f ~/.bash_aliases ]; then
+      if [ -f ~/.bash_aliases ]; then
         . ~/.bash_aliases
       fi
-      PS1="\[\033[1;33m\]\u@\h\[\033[0m\] $PS1"
+      PS1="\[\033[1;33m\]\u@\h\[\033[0m\] ➜ \w \$ "
     '';
   };
 
